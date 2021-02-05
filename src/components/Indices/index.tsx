11f290fa6b9822bcef156dfabacbe84bd6e25147
Allan Sartor <allansartor@outlook.com>
2021-02-04 21:19:55 -0300

    Feito SalesArea

--- src/components/Indices/index.tsx
+++ src/components/Indices/index.tsx
@@ -8,6 +8,7 @@ import {
   Conversao,
   TotalRecebido,
   HistoryArea,
+  EyeIcon,
 } from './style';
 
 const Indices: React.FC = () => (
@@ -17,15 +18,33 @@ const Indices: React.FC = () => (
         <div>
           <span> Total gerado </span>
         </div>
+        <div id="TotalGeradoBody">
+          <p>R$</p>
+          <span>- 1,486</span>
+          <EyeIcon />
+        </div>
       </TotalGerado>
       <Conversao>
         <div>
           <span> Conversão </span>
         </div>
+        <div id="ConversaoBody">
+          <span />
+          <span>
+            50
+            <p>%</p>
+          </span>
+          <EyeIcon />
+        </div>
       </Conversao>
       <TotalRecebido>
         <div>
           <span> Total recebido </span>
+        </div>
+        <div id="TotalRecebidoBody">
+          <p>R$</p>
+          <span>850,00</span>
+          <EyeIcon />
         </div>
       </TotalRecebido>
     </SaleArea>
--- src/components/Indices/style.ts
+++ src/components/Indices/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import * as IconSolid from '@styled-icons/ionicons-solid';
 
 export const Container = styled.div`
   display: flex;
@@ -6,6 +7,7 @@ export const Container = styled.div`
   grid-area: INDICES;
 
   align-items: center;
+  font-family: 'Poppins', sans-serif;
 `;
 export const SaleArea = styled.div`
   display: flex;
@@ -13,28 +15,137 @@ export const SaleArea = styled.div`
   flex-wrap: wrap;
   align-items: center;
 
-  background-color: white;
-
   width: 1024px;
   height: 180px;
+`;
+export const TotalGerado = styled.div`
+  width: 320px;
+  height: 100%;
 
-  > div {
-    justify-content: center;
+  background-color: var(--elem_white);
+
+  border-radius: 6px;
+
+  > div:first-child {
+    background-color: var(--elem_red);
+
     display: flex;
+    justify-content: center;
+    align-items: center;
 
     width: 320px;
-    height: 180px;
+    height: 33%;
+
+    border-radius: 6px 6px 0px 0px;
+
     > span {
-      font-size: 2rem;
+      font-size: 1.5rem;
+      font-weight: bold;
+      color: var(--elem_white);
+    }
+  }
+  #TotalGeradoBody {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 67%;
+
+    > p {
+      font-size: 1.5rem;
+    }
+    > span {
+      font-size: 2.5rem;
     }
   }
 `;
-export const TotalGerado = styled.div`
-  justify-content: center;
+export const EyeIcon = styled(IconSolid.Eye)`
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
 `;
 export const Conversao = styled.div`
-  justify-content: center;
+  width: 320px;
+  height: 100%;
+
+  background-color: var(--elem_white);
+
+  border-radius: 6px;
+
+  > div:first-child {
+    background-color: var(--elem_blue);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 320px;
+    height: 33%;
+
+    border-radius: 6px 6px 0px 0px;
+
+    > span {
+      font-size: 1.5rem;
+      font-weight: bold;
+      color: var(--elem_white);
+    }
+  }
+  #ConversaoBody {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 67%;
+
+    > span {
+      display: flex;
+      align-items: center;
+      font-size: 2.5rem;
+
+      > p {
+        font-size: 1.5rem;
+        font-weight: 500;
+      }
+    }
+  }
 `;
-export const TotalRecebido = styled.div``;
+export const TotalRecebido = styled.div`
+  width: 320px;
+  height: 100%;
+
+  background-color: var(--elem_white);
+
+  border-radius: 6px;
+
+  > div:first-child {
+    background-color: var(--elem_green);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 320px;
+    height: 33%;
+
+    border-radius: 6px 6px 0px 0px;
+
+    > span {
+      font-size: 1.5rem;
+      font-weight: bold;
+      color: var(--elem_white);
+    }
+  }
+  #TotalRecebidoBody {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 67%;
+
+    > p {
+      font-size: 1.5rem;
+    }
+    > span {
+      font-size: 2.5rem;
+    }
+  }
+`;
 export const IndicatorsArea = styled.div``;
 export const HistoryArea = styled.div``;
